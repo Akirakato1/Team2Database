019892d6d79af0d8e44e47d951b697cb1bca9494
Henry Jacobson <henryajacobson@gmail.com>
2020-12-13 16:21:29 -0600

    fix

--- src/main/java/team2/roommates/repositories/GroceryItemRepository.java
+++ src/main/java/team2/roommates/repositories/GroceryItemRepository.java
@@ -8,7 +8,7 @@ import team2.roommates.models.GroceryItem;
 import java.util.List;
 
 public interface GroceryItemRepository extends CrudRepository<GroceryItem, Integer> {
-    @Query(value = "SELECT * FROM groceryItems where apartment_id=:apartmentId", nativeQuery = true)
+    @Query(value = "SELECT * FROM grocery_items where apartment_id=:apartmentId", nativeQuery = true)
     List<GroceryItem> getGroceryItemsByApartmentId(
             @Param("apartmentId") int apartmentId
     );
